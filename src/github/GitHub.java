282b24243874215a20ca62214ac4d688a4394fca
Nick <Nick@10.250.7.41>
2017-01-19 13:41:57 -0500

    Lab1

---
+++ src/github/GitHub.java
@@ -0,0 +1,16 @@
+package github;
+
+/**
+ *
+ * @author Nick
+ */
+public class GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("Hello World, my GitHub account name is NickHolness");
+    }
+    
+}
